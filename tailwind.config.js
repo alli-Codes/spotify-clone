6a4bdd648bc54c866effa9a08ccf391369d426ba
alli-Codes <allisonevans081@gmail.com>
2023-02-20 13:51:37 +0100

    Configuring tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html, js, ts, vue}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
