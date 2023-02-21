205797a157d258bda3bc29df33ea47f13b8bc2bf
alli-Codes <allisonevans081@gmail.com>
2023-02-21 08:39:28 +0100

    Adding the Header component

---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="w-full py-5 bg-red-400 sticky top-0">
+    <p>Hello</p>
+  </header>
+</template>
--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="h-[100vh]">
+  <div class="h-[100vh] flex bg-blue-300">
     <Sidebar />
+    <div class="main w-full">
+      <Header />
+    </div>
   </div>
 </template>
