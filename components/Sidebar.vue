ff6c0d99f8bb5ac469fbda105366db86e93316fa
alli-Codes <allisonevans081@gmail.com>
2023-02-21 05:42:25 +0100

    Adding custom icons and styling them

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,37 +1,43 @@
+<script setup>
+// import { Icon } from "@iconify/vue";
+import Home from "~/components/icons/Home.vue";
+import Search from "~/components/icons/Search.vue";
+import Library from "~/components/icons/Library.vue";
+import Add from "~/components/icons/Add.vue";
+import Love from "~/components/icons/Love.vue";
+import Globe from "~/components/icons/Globe.vue";
+</script>
+
 <template>
   <div
-    class="sidebar h-full max-w-[15rem] bg-black text-white p-7 flex flex-col justify-between"
+    class="sidebar h-full max-w-[15rem] bg-black text-white px-6 py-6 flex flex-col justify-between"
   >
-    <section class="main-links flex flex-col gap-y-8">
+    <section class="main-links flex flex-col gap-y-10">
       <div class="logo w-[8rem]">
         <img src="/logo.png" alt="" />
       </div>
-      <article class="flex flex-col text-sm gap-y-2">
-        <a href="#">
-          <i></i>
+      <article class="flex flex-col text-sm font-bold gap-y-4">
+        <a href="#" class="flex items-center gap-x-4">
+          <Home />
           <p>Home</p>
         </a>
-        <a href="#">
-          <i></i>
-          <p>Home</p>
+        <a href="#" class="flex items-center gap-x-4">
+          <Search />
+          <p>Search</p>
         </a>
-        <a href="#">
-          <i></i>
-          <p>Home</p>
+        <a href="#" class="flex items-center gap-x-4">
+          <Library />
+          <p>Your Library</p>
         </a>
       </article>
-      <article class="flex flex-col gap-y-2 text-sm">
-        <a href="#">
-          <i></i>
-          <p>Home</p>
+      <article class="flex flex-col items-start gap-y-4 text-sm font-bold">
+        <a href="#" class="flex items-center gap-x-4">
+          <Add />
+          <p>Create Playlist</p>
         </a>
-        <a href="#">
-          <i></i>
-          <p>Home</p>
-        </a>
-        <a href="#">
-          <i></i>
-          <p>Home</p>
+        <a href="#" class="flex items-center gap-x-4">
+          <Love />
+          <p>Likes Songs</p>
         </a>
       </article>
     </section>
@@ -45,7 +51,12 @@
         <li><a href="">Cookies</a></li>
       </ul>
 
-      <button>English</button>
+      <button
+        class="flex items-center gap-x-2 text-sm font-bold border border-1 px-3 py-1 rounded-full"
+      >
+        <Globe />
+        English
+      </button>
     </section>
   </div>
 </template>
---
+++ components/icons/Add.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-[.4rem] bg-white rounded-sm">
+    <svg
+      role="img"
+      height="12"
+      width="12"
+      aria-hidden="true"
+      viewBox="0 0 16 16"
+      data-encore-id="icon"
+      class="Svg-sc-ytk21e-0 uPxdw"
+    >
+      <path
+        d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75z"
+        fill="gray"
+      ></path>
+    </svg>
+  </div>
+</template>
