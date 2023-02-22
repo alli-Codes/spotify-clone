5a6003e55ee618a2a56842a5db20f163426f4ce0
alli-Codes <allisonevans081@gmail.com>
2023-02-22 10:36:48 +0100

    Adding buttons

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,18 @@
 <script setup>
 // import { Icon } from "@iconify/vue";
-import Arrows from "components/icons/Arrows";
+import Arrows from "./icons/Arrows.vue";
+import Button from "./logins/Button.vue";
 </script>
 
 <template>
-  <header class="w-full py-5 bg-[#101010] sticky top-0">
+  <header
+    class="w-full px-8 py-5 bg-[#101010] flex justify-between sticky top-0"
+  >
     <div class="arrows text-white">
       <Arrows />
+    </div>
+    <div class="logins">
+      <Button />
     </div>
   </header>
 </template>
---
+++ components/logins/Button.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="px-10 py-3 bg-white rounded-full">Log in</button>
+</template>
