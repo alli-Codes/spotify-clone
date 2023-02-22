c732edc0109dd15884b9467c736226f5e56d134b
alli-Codes <allisonevans081@gmail.com>
2023-02-22 13:16:18 +0100

    Passing props

--- components/category/Category.vue
+++ components/category/Category.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Song from "./Song.vue";
+defineProps({
+  categoryName: String,
+});
 </script>
 
 <template>
   <div class="category w-full">
     <header class="flex justify-between pb-8 text-white">
-      <h1 class="font-bold text-2xl">Focus</h1>
-      <a href="#" class="text-sm">Show all</a>
+      <h1 class="font-bold text-2xl">{{ categoryName }}</h1>
+      <a href="#" class="text-sm font-bold">Show all</a>
     </header>
     <main class="w-full grid grid-flow-col gap-x-6 px-4 overflow-hidden">
       <Song />
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@ import Category from "~~/components/category/Category.vue";
 </script>
 
 <template>
-  <div class="w-full h-[300vh] px-10 py-6">
-    <Category />
+  <div class="w-full h-[300vh] px-10 py-6 flex flex-col gap-y-8">
+    <Category categoryName="Focus" />
+    <Category categoryName="Spotify Playlists" />
   </div>
 </template>
