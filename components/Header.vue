f4242d93d2ee7eb76f5faebe387474bb471fcc96
alli-Codes <allisonevans081@gmail.com>
2023-02-22 10:31:10 +0100

    Adding Arrow Icons

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,12 @@
+<script setup>
+// import { Icon } from "@iconify/vue";
+import Arrows from "components/icons/Arrows";
+</script>
+
 <template>
   <header class="w-full py-5 bg-[#101010] sticky top-0">
-    <p>Hello</p>
+    <div class="arrows text-white">
+      <Arrows />
+    </div>
   </header>
 </template>
---
+++ components/icons/Arrows.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>V</h1>
+</template>
